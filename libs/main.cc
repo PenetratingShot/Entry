dd5a2c3d774426f0cfab06f403ea3aee61018e5e
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-06 12:47:11 -0800

    Update main.cc

--- libs/main.cc
+++ libs/main.cc
@@ -2,10 +2,12 @@
 
 using namespace std;
 
+short unsigned int loop; 
+
 int main() {
   cout << "Entry";
   cout << "This is going to be the basic entry point for the code"
   return 0;
   
-  
+  // Define loop in here
 }
