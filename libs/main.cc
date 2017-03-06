a21f32bc5adda83ad31df5f4e753af4252666e8f
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-06 12:45:31 -0800

    Update main.cc

--- libs/main.cc
+++ libs/main.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 
-use mainstream std;
+using namespace std;
 
 int main() {
   cout << "Entry";
   cout << "This is going to be the basic entry point for the code"
   return 0;
+  
+  
 }
